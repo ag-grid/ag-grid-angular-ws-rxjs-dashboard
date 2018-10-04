2336e1fc96082e6d0209d106e963db5962dcfa2a
Sean Landsman <sean@thelandsmans.com>
2018-10-04 19:43:26 +0100

    Add ticker detail type

--- client/src/app/types/types.ts
+++ client/src/app/types/types.ts
@@ -1,7 +1,9 @@
 export type ROW_DATA = string[];
+
 export type MESSAGE_DATA = {
     fxData: ROW_DATA,
     tickerData: ROW_DATA
     topMovers: ROW_DATA
 }
 
+export type TICKER_DETAIL = { historicalData: {}[], pricingDelta: any, tickerSummary: any, timestamp: string }
